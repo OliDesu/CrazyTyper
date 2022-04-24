42f69cf65c7c729eca750528d9ccc0b77350179e
Ali <46926472+OliDesu@users.noreply.github.com>
2022-04-24 23:06:42 +0200

    Adding interface

---
+++ src/com/starto/Application.java
@@ -0,0 +1,33 @@
+package com.starto;
+
+import com.starto.application.Board;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Application extends JFrame {
+
+    public Application() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setSize(720, 480);
+
+        setTitle("Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            Application ex = new Application();
+            ex.setVisible(true);
+        });
+    }
+}
---
+++ src/com/starto/application/Board.java
@@ -0,0 +1,8 @@
+package com.starto.application;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+
+    public Board() {}
+}
